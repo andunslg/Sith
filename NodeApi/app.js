e562ff4cc976abd01c9592e9cbdc9d682618e0e0
sachintha <sachintha.rajith@gmail.com>
2013-06-01 22:02:09 +0530

    Added cors support

--- NodeApi/app.js
+++ NodeApi/app.js
@@ -24,6 +24,11 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.configure('development', function(){
   app.use(express.errorHandler());
 });

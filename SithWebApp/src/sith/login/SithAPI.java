1b71be02905252acf4f1248130b8c99df2cd172d
Prabhath <sumindappp@yahoo.com>
2013-06-05 00:41:13 +0530

    Added HTTP Client

---
+++ SithWebApp/src/sith/login/SithAPI.java
@@ -0,0 +1,14 @@
+package sith.login;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Prabhath
+ * Date: 6/5/13
+ * Time: 12:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SithAPI {
+
+    public static String LOGIN="";
+    public static String SIGNUP="";
+}

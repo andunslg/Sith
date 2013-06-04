a8105528ee0e634b5fa484d6815ace9fa2ac10be
Prabhath <sumindappp@yahoo.com>
2013-06-04 16:23:16 +0530

    Updated

---
+++ SithWebApp/src/sith/login/Authenticator.java
@@ -0,0 +1,21 @@
+package sith.login;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Prabhath
+ * Date: 6/4/13
+ * Time: 3:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Authenticator {
+
+    public boolean addUser(String userName,String password){
+
+        return true;
+    }
+
+
+    public boolean authenticateUser(String userName,String password){
+        return true;
+    }
+}

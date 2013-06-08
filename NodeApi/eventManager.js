f3514482ea48066ec3d1d6e83d306296061c5d65
andunslg <andunslg@gmail.com>
2013-06-08 23:49:26 +0530

    Adding eventID as a param

--- NodeApi/eventManager.js
+++ NodeApi/eventManager.js
@@ -1,12 +1,12 @@
 /**
  * @author Sachintha
  */
-exports.addEvent = function(eventName, desc, location, date, startTime, endTime,perceptionSchema){
-	doc = {eventName:eventName, description:desc, location:location, 
+exports.addEvent = function(eventID, eventName, desc, location, date, startTime, endTime,perceptionSchema){
+	doc = {eventID:eventID,eventName:eventName, description:desc, location:location,
 			date:date, startTime:startTime, endTime:endTime, perceptionSchema:perceptionSchema};
 	mongoAdapter.insertDocument('EventDetails',doc);
-	mongoAdapter.createCollection('EventPerception_'+eventName);
-	mongoAdapter.createCollection('EventUser_'+eventName);
+	mongoAdapter.createCollection('EventPerception_'+eventID);
+	mongoAdapter.createCollection('EventUser_'+eventID);
 };
 
 exports.getEvent = function(eventID){

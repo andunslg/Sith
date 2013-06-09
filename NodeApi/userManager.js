dd2b8d6bcaad97581a05fc4a8166c3d35b74c54f
sachintha <sachintha.rajith@gmail.com>
2013-06-09 21:16:12 +0530

    create user event and user perception collections while adding user

--- NodeApi/userManager.js
+++ NodeApi/userManager.js
@@ -18,6 +18,8 @@ exports.addAnnonymousUser = function(req,res){
 			res.end();
 		}else{
 			mongoAdapter.insertDocument('Users',document);
+            mongoAdapter.createCollection('UserEvent_'+req.body.userName);
+            mongoAdapter.createCollection('UserPerceptions_'+req.body.userName);
 			res.write(JSON.stringify({result:true}));
 			res.end();	
 		}
@@ -41,3 +43,19 @@ exports.authenticateUser = function(req,res){
 		}
 	});
 }
+
+exports.addUserToEvent = function(eventID,userID,fn){
+    mongoAdapter.getSingleDocument({userID:userID},'EventUser_'+eventID,function(doc){
+        if(doc){
+             fn(new Error('user already registered'));
+        }else{
+            doc1 = {userID : userID};
+            //add userID to eventuser collection(users registered for the given event)
+            mongoAdapter.insertDocument('EventUser_'+eventID,doc1);
+            //add eventID to userevent collection(all events registered by a given user)
+            doc2 = {eventID : eventID};
+            mongoAdapter.insertDocument('UserEvent_'+userID,doc2);
+            fn();
+        }
+    });
+};

4ffeda52e2c58f8c2f91eca71413f3d1c4732ea6
andunslg <andunslg@gmail.com>
2013-06-09 22:03:09 +0530

    Event_UserId table is updated when creating a Event with status admin

--- NodeApi/routes/event.js
+++ NodeApi/routes/event.js
@@ -10,7 +10,7 @@ userManager = require("../userManager");
 exports.addEvent = function(req,res){
 	eventManager.addEvent(req.body.eventID,req.body.eventName,req.body.eventAdmin, req.body.desc, req.body.location, req.body.date,
 							req.body.startTime, req.body.endTime, req.body.perceptionSchema);
-    userManager.addUserToEvent(req.body.eventID,req.body.userID,'admin')
+    userManager.addUserToEvent(req.body.eventID,req.body.eventAdmin,'admin')
 	res.writeHead(200, {'Content-Type': 'application/json'});
   	var result = JSON.stringify({response: true });
 	res.write(result);

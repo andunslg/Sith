f03d7a127b221caf6cbbc7db3d09ebe8d07a48e4
sachintha <sachintha.rajith@gmail.com>
2013-06-11 21:21:50 +0530

    added mongoclient to connect db

--- NodeApi/mongoAdapter.js
+++ NodeApi/mongoAdapter.js
@@ -3,25 +3,23 @@
  */
 var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
+var MongoClient = require('mongodb').MongoClient;
+var db1;
 //var client = new Db('Sith', new Server('192.248.8.246', 27017, {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
-
+var mongoClient = new MongoClient(new Server('192.248.8.246', 27017, {auto_reconnect: false, poolSize: 6}));
+mongoClient.open(function(err, mongoClient) {
+    db1 = mongoClient.db("Sith");
+    console.log('connected to '+ db1.databaseName);
+});
 //insert a document to the specified collection
 exports.insertDocument = function(collection,doc){
-	new Db('Sith', new Server('192.248.8.246', 27017, {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false}).open(function(err,db){
-	if(err)
-		throw err;
-	else{
-		console.log("Connected to UoM server");
-		db.collection(collection, function(err, collection) {
+		db1.collection(collection, function(err, collection) {
 			collection.insert(doc, {safe : true}, function(err, records) {
 				if(err)
 					throw err;
 				console.log("Record added as " + records[0]._id);
-				db.close();
 			});
-		});	
-	}		
-	});
+		});
 };
 //retireve the first object that matches the query
 exports.getSingleDocument = function(query,collection,fn){
@@ -29,7 +27,6 @@ exports.getSingleDocument = function(query,collection,fn){
 	if(err)
 		throw err;
 	else{
-		console.log("Connected to UoM server");
 		db.collection(collection, function(err, collection) {
 			collection.findOne(query,function(err, doc) {
 				if(err)
@@ -44,32 +41,26 @@ exports.getSingleDocument = function(query,collection,fn){
 
 //retireve multiple documents that matches the given query
 exports.getDocuments = function (query,collection,fn){
-	Db('Sith', new Server('192.248.8.246', 27017, {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false}).open(function(err,db){
-	if(err)
-		throw err;
-	else{
 		//console.log("Connected to UoM server");
-		db.collection(collection, function(err, collection) {
+		db1.collection(collection, function(err, collection) {
 			collection.find(query).toArray(function(err, doc) {
 				if(err)
 					throw err;
 					fn(doc);
-				db.close();
 			});
-		});	
-	}		
-	});
+		});
 };
 //delete a specific document from a collection
-exports.deleteDocument = function(collection,query){
+exports.deleteDocument = function(collection,query,errorFunc){
     Db('Sith', new Server('192.248.8.246', 27017, {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false}).open(function(err,db){
         if(err)
-            throw err;
+            errorFunc(new Error(err.message));
         else{
             db.collection(collection, function(err, collection) {
                 collection.remove(query,function(err, numberOfRemovedDocs) {
                     if(err)
-                        throw err;
+                        errorFunc(new Error(err.message));
+                    errorFunc(null);
                     db.close();
                 });
             });

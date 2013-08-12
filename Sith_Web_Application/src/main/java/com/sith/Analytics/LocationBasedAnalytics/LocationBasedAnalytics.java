b454b2c80aef8227ce228bdbcd9aa345abff8983
thilini <tgtshanika@gmail.com>
2013-08-12 11:34:19 +0530

    Pie chart analytics and heat map analytics are enabled

--- Sith_Web_Application/src/main/java/com/sith/Analytics/LocationBasedAnalytics/LocationBasedAnalytics.java
+++ Sith_Web_Application/src/main/java/com/sith/Analytics/LocationBasedAnalytics/LocationBasedAnalytics.java
@@ -41,22 +41,22 @@ public class LocationBasedAnalytics {
 
     public ArrayList<LocationData> getHappyCount() {
         ArrayList<LocationData> t= new ArrayList<LocationData>();
-//
-//        for(int i=0;i<10;i++){
-//            Random r = new Random();
-//            String latitude = String.valueOf(6.8111 + (7.1234 - 6.8111) * r.nextDouble());
-//            String longitude = String.valueOf(79.33 + (80.11 - 79.33) * r.nextDouble());
-//            int rm = Math.abs(r.nextInt(100));
-//            for(int j=1;j<=rm;j++){
-//                LocationData p = new LocationData(latitude,longitude);
-//
-//                t.add(p);
-//            }
-//
-//        }
-//        return t;
 
-        return getPerceptionCount("happy","1");
+        for(int i=0;i<10;i++){
+            Random r = new Random();
+            String latitude = String.valueOf(6.8111 + (7.1234 - 6.8111) * r.nextDouble());
+            String longitude = String.valueOf(79.33 + (80.11 - 79.33) * r.nextDouble());
+            int rm = Math.abs(r.nextInt(100));
+            for(int j=1;j<=rm;j++){
+                LocationData p = new LocationData(latitude,longitude);
+
+                t.add(p);
+            }
+
+        }
+        return t;
+
+//        return getPerceptionCount("happy","1");
     }
 
     public ArrayList<LocationData> getPieChartHappy() {

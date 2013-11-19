bc2166a3608bbac193bfdf90209c17b99b513628
sachintha <sachintha.rajith@gmail.com>
2013-11-19 18:50:33 +0530

    fix

--- Sith_Web_Application/src/main/java/com/sith/SithAPI.java
+++ Sith_Web_Application/src/main/java/com/sith/SithAPI.java
@@ -13,8 +13,8 @@ public class SithAPI{
 	public static SithAPI sithAPI=new SithAPI();
 	private HTTPUtil httpUtil=new HTTPUtil();
 
-	public static String NODEAPI="http://localhost:3000/";
-	public static String SOCKET_API = "http://localhost:3000";
+	public static String NODEAPI="http://192.248.15.232:3000/";
+	public static String SOCKET_API = "http://192.248.15.232:3000";
 	public static String LOGIN=NODEAPI+"authenticateUser";
 	public static String SIGNUP=NODEAPI+"registerAnnonymousUser";
 	public static String GET_EVENT_LIST=NODEAPI+"getAllEvents";
